0e69c0fd92ca37f07aaef083c929703d3a6f986c
Jordan Hollinger <jordan.hollinger@wellsky.com>
2020-08-21 00:51:54 -0400

    Move css to file, use more templates

--- src/app.js
+++ src/app.js
@@ -5,10 +5,22 @@ Vue.component('extremity', {
   template: '#extremity-template',
   methods: {
     open: function()  {
-      var modal = modals.find(x => x.id === this.modal);
-      if (modal) {
-        modal.open();
-      }
+      modals.find(x => x.id === this.modal).open();
+    },
+  }
+});
+
+Vue.component('extremity-modal', {
+  props: ['title', 'tag', 'form'],
+  template: '#extremity-modal-template',
+  methods: {
+    save: function() {
+      // TODO
+      modals.find(x => x.id === this.tag).close();
+    },
+    cancel: function() {
+      // TODO
+      modals.find(x => x.id === this.tag).close();
     },
   }
 });
@@ -18,13 +30,12 @@ var app = new Vue({
   data: {
     foo: "the data!!",
     forms: {lu: {}, ru: {}, ll: {}, rl: {}}
+  },
+  mounted: function() {
+    var modalNodes = document.querySelectorAll('.modal');
+    modals = M.Modal.init(modalNodes);
+
+    var fabNodes = document.querySelectorAll('.fixed-action-btn');
+    var fabs = M.FloatingActionButton.init(fabNodes, {direction: 'top', hoverEnabled: 'true'});
   }
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  var modalNodes = document.querySelectorAll('.modal');
-  modals = M.Modal.init(modalNodes);
-
-  var fabNodes = document.querySelectorAll('.fixed-action-btn');
-  var fabs = M.FloatingActionButton.init(fabNodes, {direction: 'top', hoverEnabled: 'true'});
 });
