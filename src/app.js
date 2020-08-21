92114b48752b78b315aa4a3a3d5ba60f2300db9b
Jordan Hollinger <jordan.hollinger@wellsky.com>
2020-08-21 15:30:12 -0400

    Improvements

--- src/app.js
+++ src/app.js
@@ -1,26 +1,49 @@
-var modals;
-
 Vue.component('extremity', {
-  props: ['title', 'color', 'form', 'modal'],
+  props: ['title', 'color', 'form'],
   template: '#extremity-template',
+  data: function() {
+    return {
+      modal: null,
+      isOpen: false,
+    };
+  },
   methods: {
-    open: function()  {
-      modals.find(x => x.id === this.modal).open();
+    open: function() {
+      this.isOpen = true;
     },
+    update: function(form) {
+      this.isOpen = false;
+      this.$emit('update:form', form);
+    },
+    remove: function() {
+      this.$emit('update:form', {added: false});
+    }
   }
 });
 
 Vue.component('extremity-modal', {
-  props: ['title', 'tag', 'form'],
+  props: ['title', 'form'],
   template: '#extremity-modal-template',
+  mounted: function() {
+    this.modal = M.Modal.init(this.$el, {dismissible: false});
+    this.modal.open();
+  },
+  destroyed: function() {
+    this.modal.close();
+  },
+  data: function() {
+    return {
+      modal: null,
+      tmpForm: Object.assign({}, this.form),
+    };
+  },
   methods: {
     save: function() {
-      // TODO
-      modals.find(x => x.id === this.tag).close();
+      this.tmpForm.added = true;
+      this.$emit('update:form', this.tmpForm);
     },
     cancel: function() {
-      // TODO
-      modals.find(x => x.id === this.tag).close();
+      this.$emit('cancel');
     },
   }
 });
@@ -29,11 +52,16 @@ var app = new Vue({
   el: '#app',
   data: {
     foo: "the data!!",
-    forms: {lu: {}, ru: {}, ll: {}, rl: {}}
+    forms: {cn: {added: false}, lu: {added: false}, ru: {added: false}, ll: {added: false}, rl: {added: false}}
+  },
+  methods: {
+    clear: function() {
+      this.forms = {cn: {added: false}, lu: {added: false}, ru: {added: false}, ll: {added: false}, rl: {added: false}};
+    }
   },
   mounted: function() {
-    var modalNodes = document.querySelectorAll('.modal');
-    modals = M.Modal.init(modalNodes);
+    var modalNode = document.getElementById('modal-final');
+    var modal = M.Modal.init(modalNode);
 
     var fabNodes = document.querySelectorAll('.fixed-action-btn');
     var fabs = M.FloatingActionButton.init(fabNodes, {direction: 'top', hoverEnabled: 'true'});
